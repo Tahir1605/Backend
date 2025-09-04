9cddff9d99e075ece7fbaf46cc1c40150187467d
Tahirul Islam <5566tahirul@gmail.com>
2025-09-04 09:53:31 +0530

    new backend practices

---
+++ 02Backend/routes/clientRoutes.js
@@ -0,0 +1,12 @@
+import express from 'express';
+import upload from '../middlewares/multer.js';
+import { addClient , deleteClient, fetchClient, updateClient } from '../controllers/clientController.js';
+
+const clientRouter = express.Router();
+
+clientRouter.post("/add",upload.single('image'),addClient)
+clientRouter.get("/fetch",fetchClient)
+clientRouter.delete("/delete/:id",deleteClient)
+clientRouter.put("/update/:id",upload.single('image'),updateClient)
+
+export default clientRouter
